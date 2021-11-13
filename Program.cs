e15fe108eaea47c83f33fb553882ed023f822752
unknown <shaunak12wankhade@ggmail.com>
2021-11-13 16:04:30 +0530

    [Shaunak] Added : Retrieve emp payroll data from database

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace PayrollServicesUsingADO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to payroll services using ADO");
+            //Console.WriteLine("Welcome to payroll services using ADO");
+
+            EmployeeRepository repository = new EmployeeRepository();
+            Console.WriteLine("Id Name BasicPay Startdate Gender Department PhoneNumber Address Deduction Tax IncomeTax NetPay \n");
+            repository.GetAllEmployee();
+            Console.ReadLine();
 
         }
     }

3a6086e974e7c3cacd0d708b2f56d05b48741a1a
unknown <shaunak12wankhade@ggmail.com>
2021-11-13 14:22:45 +0530

    [Shaunak] Added : Welcome message

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PayrollServicesUsingADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to payroll services using ADO");
+
+        }
+    }
+}
